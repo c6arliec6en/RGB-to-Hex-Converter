2dcb95cec3c202a61652553a2b77180e7fa90ad9
Charlie <chen_610146@hotmail.com>
2019-03-11 21:30:27 +0800

    Color Bar

--- main.js
+++ main.js
@@ -70,3 +70,92 @@ function rgbConvertHex  (r, g, b) {
 
 }
 
+
+//Color Bar
+
+
+function rgbConvertHexBar  (color) {
+	let hex = ''
+	let Interger = Math.floor(color/16)
+	let remainder = color%16
+	let colorCode = [Interger, remainder]
+
+	colorCode.forEach(num => {
+		if (num > 9) {
+			switch (num) {
+				case 10:
+					hex += 'A'
+					break;
+				case 11:
+					hex += 'B'
+					break;
+				case 12:
+					hex += 'C'
+					break;
+				case 13:
+					hex += 'D'
+					break;
+				case 14:
+					hex += 'E'
+					break;
+				case 15:
+					hex += 'F'
+					break;					
+				default:
+					break;
+				}
+			} else if (num === 0){
+					hex += '0'
+			} else {
+				hex += num
+			}
+		})
+
+	return hex
+}
+
+
+
+const colorBars = document.querySelectorAll('.bar')
+const colorDisplay = document.querySelector('.color-display')
+const hexDisplay = document.querySelector('.hex-display')
+const content = document.querySelector('body')
+const hexBar = ['FF','FF','FF']
+
+function showTheColor () {
+	let background = '#' + hexBar[0] + hexBar[1] + hexBar[2]
+	hexDisplay.innerHTML = background 
+	content.style.backgroundColor = background
+}
+
+
+
+
+function getTheValue () {
+	let value = this.value
+	
+
+	// console.log(this)
+	this.nextElementSibling.innerHTML = value
+
+	if (this.matches('#red-bar')) {
+		hexBar[0] = rgbConvertHexBar(value)
+		console.log(hexBar)
+		console.log('cool')
+		showTheColor () 
+	}
+
+	if (this.matches('#green-bar')) {
+		console.log('good')
+	}
+
+	if (this.matches('#black-bar')) {
+		console.log('333')
+	}
+
+
+}
+colorBars.forEach(colorBar => colorBar.addEventListener('change',getTheValue))
+colorBars.forEach(colorBar => colorBar.addEventListener('mousemove',getTheValue))
+
+
